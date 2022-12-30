d185a26a2942afe961a969de6e80bf62ffba242f
Billy Cook <billy.cook12345@gmail.com>
2022-12-30 18:38:06 -0500

    Re-added generic Credentials file.

---
+++ NetworkMonitor/credentials.py
@@ -0,0 +1,5 @@
+# Email credentials
+# Database credentials
+
+
+SAMPLE_NMAP_OUTPUT = 'Starting Nmap 7.70 ( https://nmap.org ) at 2022-12-30 17:48 EST\nNmap scan report for 192.168.99.99\nHost is up (0.0089s latency).\nMAC Address: 44:44:44:44:44:44 (TestManufacturer1)\nNmap scan report for TestDevice1 (192.168.99.999)\nHost is up (0.29s latency).\nMAC Address: 44:44:44:44:44:44 (TestManufacturer2)\nNmap done: 256 IP addresses (2 hosts up) scanned in 5.06 seconds\n'
